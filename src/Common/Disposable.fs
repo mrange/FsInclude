13a0bc7288e12b1f3a9bdcfb4be5e553e6e3e140
mrange <marten_range@hotmail.com>
2015-01-25 15:17:12 +0100

    Added Common/Disposable

---
+++ src/Common/Disposable.fs
@@ -0,0 +1,41 @@
+// Copyright 2015 Mårten Rånge
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// ### INCLUDE: Common.fs
+
+namespace FsInclude
+
+[<AbstractClass>]
+type BaseDisposable() =
+    [<DefaultValue>]
+    val mutable isDisposed : int
+
+    interface System.IDisposable with
+        member x.Dispose () = 
+            if System.Threading.Interlocked.Exchange (&x.isDisposed, 1) = 0 then
+                try
+                    x.OnDispose ()
+                with 
+                | e -> Log.errorf "%s.Dispose () threw exception: %A" "" e
+
+    abstract OnDispose: unit -> unit
+
+type ActionDisposable(action : unit -> unit) =
+    inherit BaseDisposable()
+
+    override x.OnDispose () = action ()
+
+module Disposable = 
+    let onExitDo (action : unit -> unit) : System.IDisposable = upcast new ActionDisposable(action)
+
--- src/FsInclude/FsInclude.fs
+++ src/FsInclude/FsInclude.fs
@@ -1,4 +1,18 @@
-﻿namespace FsInclude
+﻿// Copyright 2015 Mårten Rånge
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace FsInclude
 
 module Processor = 
 
@@ -100,7 +114,7 @@ module Processor =
 
         [|
             """^.*$"""                                          , defaultAction
-            """^\s*//\s*###\s*INCLUDE:\s*(?<ref>\S+)\s*.*$"""   , includeAction
+            """^\s*//\s+###\s*INCLUDE:\s*(?<ref>\S+)\s*.*$"""   , includeAction
             """^(?<ws>\s*)namespace\s+(?<ns>\S+)\s*.*$"""       , namespaceAction
             """^(?<ws>\s*)open\s+(?<ns>\S+)\s*.*$"""            , openAction
         |] |> Array.map (fun (r,a) -> makeRegex r, a)
