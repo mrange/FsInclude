43eddfcdb5ea45ae8bd3667929a41242bbc719c6
mrange <marten_range@hotmail.com>
2015-01-29 22:03:07 +0100

    Streams

--- src/Modules/StreamModule.fs
+++ src/Modules/StreamModule.fs
@@ -14,24 +14,25 @@
 
 namespace FsInclude
 
-type RReceiver<'T>  = 'T -> bool 
-type RStream<'T>    = RReceiver<'T> -> unit
+module Stream =
 
-module RStream =
+    type Receiver<'T>   = 'T -> bool 
+    type Stream<'T>     = Receiver<'T> -> unit
 
-    let inline filter (f : 'T -> bool) (s : RStream<'T>) : RStream<'T> = 
+
+    let inline filter (f : 'T -> bool) (s : Stream<'T>) : Stream<'T> = 
         fun receiver ->
             let r v = 
                 if (f v) then receiver v
                 else true
             s r
 
-    let inline map (f : 'T -> 'U) (s : RStream<'T>) : RStream<'U> = 
+    let inline map (f : 'T -> 'U) (s : Stream<'T>) : Stream<'U> = 
         fun receiver ->
             let r v = receiver (f v)
             s r
 
-    let inline ofType (s : RStream<'T>) : RStream<'U> = 
+    let inline ofType (s : Stream<'T>) : Stream<'U> = 
         fun receiver ->
             let r (v : 'T) =
                 match box v with
@@ -39,7 +40,7 @@ module RStream =
                 | _ -> true
             s r
 
-    let inline take (n : int) (s : RStream<'T>) : RStream<'T> = 
+    let inline take (n : int) (s : Stream<'T>) : Stream<'T> = 
         fun receiver ->
             let rn = ref n
             let r v = 
@@ -50,7 +51,7 @@ module RStream =
                     false
             s r
 
-    let inline skip (n : int) (s : RStream<'T>) : RStream<'T> = 
+    let inline skip (n : int) (s : Stream<'T>) : Stream<'T> = 
         fun receiver ->
             let rn = ref n
             let r v = 
@@ -61,50 +62,50 @@ module RStream =
                     receiver v
             s r
 
-    let inline toArray (s : RStream<'T>) : 'T [] = 
+    let inline toArray (s : Stream<'T>) : 'T [] = 
         let ra = ResizeArray<_> ()
         let r v = ra.Add v; true
         s r
         ra.ToArray ()
 
-    let inline toList (s : RStream<'T>) : 'T list = 
+    let inline toList (s : Stream<'T>) : 'T list = 
         let l = ref List.empty
         let r v = l := v::!l; true
         s r
         List.rev !l
 
-    let inline toSum (initial : 'T) (s : RStream<'T>) : 'T = 
+    let inline toSum (initial : 'T) (s : Stream<'T>) : 'T = 
         let sum = ref initial
         let r v = sum := !sum + v; true
         s r
         !sum
 
-    let inline ofRange (f : 'T) (inc : 'T) (t : 'T) : RStream<'T> =
+    let inline ofRange (f : 'T) (inc : 'T) (t : 'T) : Stream<'T> =
         fun receiver ->
             let mutable i = f
             while i < t && (receiver i) do
                 i <- i + inc
 
-    let inline ofArray (a : 'T []) : RStream<'T> =
+    let inline ofArray (a : 'T []) : Stream<'T> =
         fun receiver ->
             let l = a.Length
             let mutable i = 0
             while i < l && (receiver a.[i]) do
                 i <- i + 1
 
-    let inline ofList (l : 'T list) : RStream<'T> =
+    let inline ofList (l : 'T list) : Stream<'T> =
         fun receiver ->
             let mutable i = l
             while i.IsEmpty && (receiver i.Head) do
                 i <- i.Tail
 
-    let inline ofSeq (l : seq<'T>) : RStream<'T> =
+    let inline ofSeq (l : seq<'T>) : Stream<'T> =
         fun receiver ->
             use e = l.GetEnumerator ()
             while e.MoveNext () && (receiver e.Current) do
                 ()
 
-    let inline firstOrValue (v : 'T) (s : RStream<'T>) : 'T =
+    let inline firstOrValue (v : 'T) (s : Stream<'T>) : 'T =
         let rv = ref v
         let a v =
             rv := v
@@ -112,7 +113,7 @@ module RStream =
         s a
         !rv
 
-    let inline first (s : RStream<'T>) : 'T option =
+    let inline first (s : Stream<'T>) : 'T option =
         let rv = ref None
         let a v =
             rv := Some v
@@ -120,6 +121,7 @@ module RStream =
         s a
         !rv
 
+(*
 
 type FReceiver<'T> =
     interface
@@ -176,3 +178,4 @@ module FStream =
 
         s r
         !sum
+*)
